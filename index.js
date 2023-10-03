f02fafd11944875e73ee8db8752e9c8f6db9e6d3
Gabo <jvsoftassetstore@gmail.com>
2023-10-03 16:01:46 -0400

    refactored and changed the order

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-// import the express application
-import express from 'express';
-import compression from 'compression';
+import express from 'express'; // Express.js app
+import compression from 'compression'; // compression
 
 import { fileURLToPath } from 'url';
 import { dirname, sep } from 'path';
 
-const __dirname = dirname(fileURLToPath( import.meta.url)) + sep;
-
 // configuration
-const cfg =  {
+const __dirname = dirname(fileURLToPath( import.meta.url)) + sep,
+    cfg =  {
     port: process.env.port || 3000,
     dir: {
         root: __dirname,
@@ -25,26 +23,26 @@ const app = express();
 // do not identify Express
 app.disable('x-powered-by');
 
+// use the compression middleware
+app.use(compression());
+
 // console.log each request url (middleware)
 app.use((req, res, next) => {
     console.log(req.url);
     next();
 });
 
-// use the compression middleware
-app.use(compression());
-
 // home page route
 app.get('/', (req, res) => {
     res.send('Hello G!');
 });
 
-// /hello route
+// another route
 app.get('/hello/', (req, res) => {
     res.send(`Hello from the ${req.url} path`);
 });
 
-// server static files
+// server static assets
 app.use(express.static( cfg.dir.static ));
 
 // 404 error
